8e1a7f3ae57db1c46a3202b3bc0392931d6ec99e
RestrictedWord <68745274+RestrictedWord@users.noreply.github.com>
2023-01-03 19:00:22 -0800

    dasadsasdsadads
    dasdasdasdasdfhgkjrdfhkgjhsdkjhfjkadhfjkhfkjhfjdhjkfdshjfhjjfhdjfhsjdfjhdsjfsdhfhjksdfhjkdshfjsdhfjdshIRRVLOLIKESMENasdfhasjkdhasjhdjkasdhjksa

--- src/main.js
+++ src/main.js
@@ -1,4 +1,13 @@
 window.addEventListener("DOMContentLoaded", () => {
+  // Fetch CSS
+  fetch("https://namelessclient.xyz/src/assets/css/nlc-theme.css")
+    .then((response) => {
+      return response.text();
+    })
+    .then((css) => {
+      theme.innerHTML = css;
+      document.head.appendChild(theme);
+    });
   // Create DOM elements
   const theme = document.createElement("style");
   const nlc = document.createElement("div");
@@ -6,9 +15,9 @@ window.addEventListener("DOMContentLoaded", () => {
   const clientBtn = document.createElement("button");
   const discordBtn = document.createElement("button");
   const discordInvite = document.createElement("style");
-
   // Set DOM element attributes
   nlc.className = "NLC-Elements";
+  clientBtn.id = "Client-Btn";
   clientBtn.className = "Client-Btn nlc-btn";
   clientBtn.innerText = "GET CLIENT";
   discordBtn.id = "Discord-Btn";
@@ -36,6 +45,7 @@ window.addEventListener("DOMContentLoaded", () => {
     transition: all .3s ease;
     cursor: pointer;
     box-shadow: var(--shadow-primary);
+    text-transform: uppercase;
 }
 .Client-Btn{
     color: var(--text-clr-secondary-3);
@@ -44,10 +54,53 @@ window.addEventListener("DOMContentLoaded", () => {
 .Discord-Btn{
     color: var(--background-clr-secondary-6);
 }
+.NLE-Settings{
+  color: var(--background-clr-secondary-7);
+}
 .nlc-btn:hover{
     background: var(--background-clr-secondary-1);
     color: var(--text-clr-primary);
     padding: 0.875rem 0.875rem;
+}
+.Settings-Modal {
+  position: fixed;
+  flex-direction: column;
+  top: 9vh;
+  width: 742px;
+  height: 800px;
+  left: 50%;
+  transform: translate(-50%, 0);
+  background: var(--background-clr-primary);
+  border-radius: 0.25rem;
+  padding: 0.5rem;
+}
+.Close-Btn{
+  background: var(--background-clr-secondary-7);
+  width: 30px;
+  height: 30px;
+  border-radius: 0.25rem;
+  margin-bottom: 0.5rem;
+  border: 0;
+}
+.Settings-Modal-Bar{
+  display: flex;
+  align-items: flex-start;
+  justify-content: space-between;
+  flex-direction: row-reverse;
+  width: 100%;
+  height: min-content;
+  border-radius: 0.25rem;
+}
+.Settings-Modal-Content{
+  width: 100%;
+  height: 100%;
+}
+.Settings-Modal-Left{
+  height: 100%;
+  color: var(--text-clr-primary);
+  text-transform: uppercase;
+  font-weight: 600;
+  font-size: 1.313rem;
 }
   `;
   discordInvite.innerHTML = `
@@ -62,15 +115,6 @@ window.addEventListener("DOMContentLoaded", () => {
     display: none;
   }
   `;
-  // Fetch CSS
-  fetch("https://namelessclient.xyz/src/assets/css/nlc-theme.css")
-    .then((response) => {
-      return response.text();
-    })
-    .then((css) => {
-      theme.innerHTML = css;
-      document.head.appendChild(theme);
-    });
 
   // Create Dom function
   discordBtn.addEventListener("click", () => {
@@ -89,6 +133,7 @@ window.addEventListener("DOMContentLoaded", () => {
     // Open the URL in a new window
     window.open("https://namelessclient.xyz/");
   });
+
   // Append elements to the DOM
   document.head.appendChild(discordInvite);
   document.head.appendChild(nlcTheme);
